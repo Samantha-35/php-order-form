61d5ba7710c05fdaba990cba812a2d386b79d4d5
Samantha <ramakerssamantha1@gmail.com>
2021-03-18 14:33:21 +0100

    Update on the index

---
+++ src/form-view.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Order Pizzas & drinks</title>
+</head>
+<body>
+<div class="container">
+    <h1>Order pizzas in restaurant "the Personal Pizza Processors"</h1>
+    <nav>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="?food=1">Order pizzas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?food=0">Order drinks</a>
+            </li>
+        </ul>
+    </nav>
+    <form method="post">
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="email">E-mail:</label>
+                <input type="text" id="email" name="email" class="form-control"/>
+            </div>
+            <div></div>
+        </div>
+
+        <fieldset>
+            <legend>Address</legend>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="street">Street:</label>
+                    <input type="text" name="street" id="street" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="streetnumber">Street number:</label>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="city">City:</label>
+                    <input type="text" id="city" name="city" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="zipcode">Zipcode</label>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control">
+                </div>
+            </div>
+        </fieldset>
+
+        <fieldset>
+            <legend>Products</legend>
+            <?php foreach ($products AS $i => $product): ?>
+                <label>
+                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
+                    &euro; <?php echo number_format($product['price'], 2) ?></label><br />
+            <?php endforeach; ?>
+        </fieldset>
+        
+        <label>
+            <input type="checkbox" name="express_delivery" value="5" /> 
+            Express delivery (+ 5 EUR) 
+        </label>
+            
+        <button type="submit" class="btn btn-primary">Order!</button>
+    </form>
+
+    <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in pizza(s) and drinks.</footer>
+</div>
+
+<style>
+    footer {
+        text-align: center;
+    }
+</style>
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>The Personal Pizza Processor!</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<main>
+<p>Order your pizza now!</p>
+<form action="contactform.php" method="post" class="contact-form">
+<input type="text" name="Email" placeholder="Your email">
+<input type="text" name="Street" placeholder="Your street">
+<input type="text" name="Street number" placeholder="Your street number">
+<input type="text" name="City" placeholder="Your city">
+<input type="text" name="Zipcode" placeholder="Your zipcode">
+</form>
+</main>
+</body>
+</html>
+<?php
+//this line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+//we are going to use session variables so we need to enable sessions
+session_start();
+
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+//your products with their price.
+$products = [
+    ['name' => 'Margherita', 'price' => 8],
+    ['name' => 'Hawaï', 'price' => 8.5],
+    ['name' => 'Salami pepper', 'price' => 10],
+    ['name' => 'Prosciutto', 'price' => 9],
+    ['name' => 'Parmiggiana', 'price' => 9],
+    ['name' => 'Vegetarian', 'price' => 8.5],
+    ['name' => 'Four cheeses', 'price' => 10],
+    ['name' => 'Four seasons', 'price' => 10.5],
+    ['name' => 'Scampi', 'price' => 11.5]
+];
+
+$products = [
+    ['name' => 'Water', 'price' => 1.8],
+    ['name' => 'Sparkling water', 'price' => 1.8],
+    ['name' => 'Cola', 'price' => 2],
+    ['name' => 'Fanta', 'price' => 2],
+    ['name' => 'Sprite', 'price' => 2],
+    ['name' => 'Ice-tea', 'price' => 2.2],
+];
+
+$totalValue = 0;
+
+require 'form-view.php';
